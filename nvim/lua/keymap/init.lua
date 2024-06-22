523ad0b2ff024cc65224b9eac3b890239913f6cb
Alex Burdusel <alexandru.burdusel@gmail.com>
2024-06-22 16:36:57 +0300

    Dynamically nvim keymaps and update midnight commander skin from tmux

--- nvim/lua/keymap/init.lua
+++ nvim/lua/keymap/init.lua
@@ -1,13 +1,15 @@
-local current_dir = vim.fn.expand("<sfile>:p:h")
-local vim_files = vim.fn.glob(current_dir .. "/**/*.vim", true, true)
-for _, file in ipairs(vim_files) do
-  vim.cmd("source " .. file)
-end
+-- Loads all the keymap files in the current directory and its subdirectories
 
-local lua_files = vim.fn.glob(current_dir .. "/**/*.lua", true, true)
-for _, file in ipairs(lua_files) do
-  if file:match("keymap/init.lua") == nil then
-  --  dofile(file)
+local current_dir = vim.fn.expand("<sfile>:p:h")
+local files = vim.fn.glob(current_dir .. "/**/*", true, true)
+for _, file in ipairs(files) do
+  if file:match("%.vim$") then
+    vim.cmd("source " .. file)
+  elseif file:match("%.lua$") then
+    -- if the file is not init.lua
+    if not file:match("init%.lua$") then
+      dofile(file)
+    end
   end
 end
 

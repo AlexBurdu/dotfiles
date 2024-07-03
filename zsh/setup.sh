685377812470e35f27fe79daaedde2a8cb4033ae
Alex <alexburdusel@google.com>
2024-07-03 20:49:30 +0300

    Ask the user whether to copy or symlink zshrc

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 # Clone themes & plugins
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone --depth=1 https://github.com/marlonrichert/zsh-autocomplete.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autocomplete
@@ -7,4 +6,10 @@ git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.o
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-autosuggestions
 
 rm -rf ~/.zshrc
-ln -s $(pwd)/zshrc ~/.zshrc
+echo "Do you want to copy or symlink ~/.zshrc? (c/s)"
+read -r response
+if [ "$response" = "c" ]; then
+  cp $(pwd)/zshrc ~/.zshrc
+elif [ "$response" = "s" ]; then
+  ln -s $(pwd)/zshrc ~/.zshrc
+fi

1de09668aa9312e424faeb97d4ad4d4eee4ab66d
Alex Burdusel <alexandru.burdusel@gmail.com>
2024-07-03 06:52:12 +0300

    Add zsh plugins clonning to setup script

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
+# Clone themes & plugins
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone --depth=1 https://github.com/marlonrichert/zsh-autocomplete.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autocomplete
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-autosuggestions
+
 rm -rf ~/.zshrc
 ln -s $(pwd)/zshrc ~/.zshrc

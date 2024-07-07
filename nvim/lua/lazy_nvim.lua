93cfeffc69e97ab3d01fc6abab1cc090b4006138
Alex Burdusel <alexandru.burdusel@gmail.com>
2024-07-07 18:21:42 +0300

    Update key bindings to better resemble ideavim ones

--- nvim/lua/lazy_nvim.lua
+++ nvim/lua/lazy_nvim.lua
@@ -18,6 +18,11 @@ require("lazy").setup({
     -- import your plugins
     { import = "plugins" },
   },
+  change_detection = {
+    -- automatically check for config file changes and reload the ui
+    enabled = true,
+    notify = false, -- get a notification when changes are found
+  },
   -- automatically check for plugin updates
   checker = { enabled = false },
 })

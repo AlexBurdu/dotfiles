848b974474ab5800d7e173390c6272024780d571
Alex Burdușel <alex.burdusel@gmail.com>
2026-02-19 16:19:06 +0200

    Fix cursor positioning in review comment keybinding

--- nvim/lua/keymap/review_comment.lua
+++ nvim/lua/keymap/review_comment.lua
@@ -1,12 +1,16 @@
 -- Insert review comment markers [>> ]
 -- Normal mode: inserts [>> ] with cursor after ">> "
--- Visual mode: wraps selection as [selected text >> ] with cursor after ">> "
+-- Visual mode: wraps selection as [selected text >> ]
+--   with cursor after ">> "
 
 vim.keymap.set('n', '<Leader><Leader>c', function()
-  vim.api.nvim_put({'[>> ]'}, 'c', false, false)
-  -- Position cursor between ">> " and "]"
-  local col = vim.fn.col('.')
-  vim.api.nvim_win_set_cursor(0, {vim.fn.line('.'), col + 3})
+  local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+  local line = vim.api.nvim_get_current_line()
+  local before = line:sub(1, col)
+  local after = line:sub(col + 1)
+  vim.api.nvim_set_current_line(before .. '[>> ]' .. after)
+  -- Cursor on the space before ]
+  vim.api.nvim_win_set_cursor(0, {row, col + 4})
   vim.cmd('startinsert')
 end, { desc = 'Insert review comment' })
 
@@ -16,10 +20,13 @@ vim.keymap.set('v', '<Leader><Leader>c', function()
   local text = vim.fn.getreg('z')
   -- Delete the selection
   vim.cmd('normal! gv"_d')
-  -- Insert wrapped text
-  vim.api.nvim_put({'[' .. text .. ' >> ]'}, 'c', false, false)
-  -- Position cursor between ">> " and "]"
-  local col = vim.fn.col('.')
-  vim.api.nvim_win_set_cursor(0, {vim.fn.line('.'), col + #text + 5})
+  local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+  local line = vim.api.nvim_get_current_line()
+  local before = line:sub(1, col)
+  local after = line:sub(col + 1)
+  local insert = '[' .. text .. ' >> ]'
+  vim.api.nvim_set_current_line(before .. insert .. after)
+  -- Cursor on the space before ]
+  vim.api.nvim_win_set_cursor(0, {row, col + #insert - 1})
   vim.cmd('startinsert')
 end, { desc = 'Wrap selection in review comment' })

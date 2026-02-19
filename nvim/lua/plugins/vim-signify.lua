df70091a198fe074eebf8bd27879221e55a67ba6
Alex Burdușel <alex.burdusel@gmail.com>
2026-02-19 09:39:38 +0200

    Add Leader-vn/vN bindings for next/previous change hunk in nvim

--- nvim/lua/plugins/vim-signify.lua
+++ nvim/lua/plugins/vim-signify.lua
@@ -2,5 +2,7 @@ return {
   "mhinz/vim-signify",
   config = function()
     vim.keymap.set('n', '<Leader>vd', ':SignifyHunkDiff<CR>', { desc = 'Show hunk diff' })
+    vim.keymap.set('n', '<Leader>vn', '<plug>(signify-next-hunk)', { desc = 'Next change hunk' })
+    vim.keymap.set('n', '<Leader>vN', '<plug>(signify-prev-hunk)', { desc = 'Previous change hunk' })
   end
 }

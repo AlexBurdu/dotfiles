80b6f1f573bde6be099eb2d326ac3d6421b8692d
Alex Burdușel <alex.burdusel@gmail.com>
2026-02-23 17:26:45 +0200

    Use LSP formatting for <Leader><Leader>f with indent fallback, expand editorconfig

--- nvim/lua/keymap/lsp.lua
+++ nvim/lua/keymap/lsp.lua
@@ -3,3 +3,13 @@ vim.keymap.set('n', '<Leader>se', vim.diagnostic.open_float, { desc = 'Show erro
 vim.keymap.set('n', '<Leader>sh', vim.lsp.buf.hover, { desc = 'Show hover documentation' })
 vim.keymap.set('n', '<Leader>n', vim.diagnostic.goto_next, { desc = 'Next diagnostic' })
 vim.keymap.set('n', '<Leader>N', vim.diagnostic.goto_prev, { desc = 'Previous diagnostic' })
+
+vim.keymap.set('n', '<Leader><Leader>f', function()
+  local clients = vim.lsp.get_clients({ bufnr = 0, method = 'textDocument/formatting' })
+  if #clients > 0 then
+    vim.lsp.buf.format()
+  else
+    vim.cmd('normal! gg=G\15')  -- \15 = <C-o>
+  end
+  vim.cmd('normal! zz')
+end, { desc = 'Format file (LSP or indent fallback)' })

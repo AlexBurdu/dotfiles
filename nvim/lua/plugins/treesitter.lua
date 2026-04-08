bfc1f5ff4afb5ee8d89d945ec7f3518c98777bb5
Alex Burdușel <alex.burdusel@gmail.com>
2026-04-09 00:35:41 +0300

    Update treesitter to main branch with new API
    Pin to commit 4916d65 (2026-04-03) which uses the new
    require("nvim-treesitter").setup() API. The old configs
    module was removed upstream.

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,58 +1,17 @@
 -- Tree-sitter is a parser generator tool that produces parsers for programming
 -- languages.
--- In other words, it allows Neovim to understand the structure of code, allowing 
+-- In other words, it allows Neovim to understand the structure of code, allowing
 -- for better syntax highlighting, code folding, and other features.
 -- https://github.com/nvim-treesitter/nvim-treesitter
 return {
   "nvim-treesitter/nvim-treesitter",
+  commit = "4916d6592ede8c07973490d9322f187e07dfefac",
   build = ":TSUpdate",
   config = function()
-    require("nvim-treesitter.configs").setup({
-      -- A list of parser names, or "all"
+    require("nvim-treesitter").setup({
       ensure_installed = "all",
       ignore_install = { "systemverilog" },
-      --{
-      --  "bash",
-      --  "c",
-      --  "cmake",
-      --  "cpp",
-      --  "css",
-      --  "csv",
-      --  "diff",
-      --  "dockerfile",
-      --  "java",
-      --  "kotlin",
-      --  "lua",
-      --  "markdown",
-      --  "markdown_inline",
-      --  "proto",
-      --  "python",
-      --  "starlark",
-      --  "vimdoc",
-      --  "yaml"
-      --},
-
-      -- Install parsers synchronously (only applied to `ensure_installed`)
-      sync_install = false,
-
-      -- Automatically install missing parsers when entering buffer
-      -- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
       auto_install = true,
-
-      indent = {
-        enable = true
-      },
-
-      highlight = {
-        -- `false` will disable the whole extension
-        enable = true,
-
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
-        additional_vim_regex_highlighting = { "markdown" },
-      },
     })
   end
 }
